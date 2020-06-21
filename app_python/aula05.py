d3ec1a966c75be99762ed9b48749977d19b62f9b
Diego Neves <pdcassiano@gmail.com>
2020-06-20 22:47:29 -0300

    Introdução ao Python aula 4

---
+++ app_python/aula05.py
@@ -0,0 +1,63 @@
+lista = [12, 1, 3, 10, 5, 7]
+lista_animal = ['cachorro', 'gato', 'elefante', 'lobo', 'arara']
+
+tupla = (1, 1, 10, 12, 14)
+
+print(tupla)
+print(len(tupla))
+print(len(lista_animal))
+
+tupla_animal = tuple(lista_animal)
+
+print(type(tupla_animal))
+print(tupla_animal)
+
+lista_numerica = list(tupla)
+print(type(lista_numerica))
+print(lista_numerica)
+
+# lista.sort()
+# lista_animal.sort()
+
+# print(lista)
+# print(lista_animal)
+# lista_animal.reverse()
+# print(lista_animal)
+
+
+# print(lista)
+# print(lista_animal[1])
+# nova_lista = lista_animal * 3
+# print(nova_lista)
+
+# soma = 0
+# for x in lista:
+#   print(x)
+#   soma += x
+# print(soma)
+
+# print(sum(lista))
+# print(max(lista))
+# print(min(lista))
+# print(min(lista_animal))
+# print(max(lista_animal))
+
+# if 'gato' in lista_animal:
+#   print('Existe um gato na lista')
+# else:
+#   print('não existe um gato na lista')
+
+# if 'lobo' in lista_animal:
+#   print('Existe um lobo na lista')
+# else:
+#   print('não existe um lobo na lista. Será incluído')
+#   lista_animal.append('lobo')
+#   print(lista_animal)
+  
+# lista_animal.pop()
+# print(lista_animal)
+# # lista_animal.pop(0)
+# # print(lista_animal)
+# lista_animal.remove('gato')
+# print(lista_animal)
+
