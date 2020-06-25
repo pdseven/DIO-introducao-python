8f93a2fce1d8715c9ba6e517e0c47f5d6d78a794
Diego Neves <pdcassiano@gmail.com>
2020-06-25 09:35:44 -0300

    aula de funções em python

---
+++ app_python/aula7_televisao.py
@@ -0,0 +1,36 @@
+class Televisao:
+    def __init__(self):
+        self.ligada = False
+        self.canal = 5
+
+    def power(self):
+        self.ligada = not self.ligada
+
+    def aumenta_canal(self):
+        if self.ligada:
+            self.canal += 1
+        else:
+            print('A Televisão está deslida')
+
+    def diminui_canal(self):
+        if self.ligada:
+            self.canal -= 1
+        else:
+            print('A Televisão está deslida')
+
+
+televisao = Televisao()
+print('Televisão está ligada: {}'.format(televisao.ligada))
+televisao.power()
+print('Televisão está ligada: {}'.format(televisao.ligada))
+televisao.power()
+print('Televisão está ligada: {}'.format(televisao.ligada))
+print('Canal: {}'.format(televisao.canal))
+televisao.aumenta_canal()
+televisao.power()
+print('Televisão está ligada: {}'.format(televisao.ligada))
+televisao.aumenta_canal()
+televisao.aumenta_canal()
+print('Canal: {}'.format(televisao.canal))
+televisao.diminui_canal()
+print('Canal: {}'.format(televisao.canal))
