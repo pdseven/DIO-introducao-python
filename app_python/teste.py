2d8f98b765de0a4d3ed0e03d80ecefa0c184847f
Diego Neves <pdcassiano@gmail.com>
2020-06-26 22:49:09 -0300

    aula de instalação de pacotes em python, conclusão do curso

--- app_python/teste.py
+++ app_python/teste.py
@@ -1,14 +1,5 @@
-class InputError(Exception):
-    def __init__(self, message):
-        self.message = message
-
-nome = 'Adda'
-while True:
-    try:
-        x = input('Digite um nome: ')
-        if x == nome:
-            break
-        else:
-            raise InputError('Nome inválido')
-    except InputError as ex:
-        print(ex)
+import requests
+url = 'https://digitalinnovation.one'
+response = requests.get(url)
+status_code = response.status_code
+print(status_code)
